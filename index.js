63d16594dce94132ede51fd94755c834c5036624
Murilo <ferrarimurilo10@gmail.com>
2024-07-29 20:38:05 -0300

    Teste

---
+++ index.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")({sigint:true});
+
+while(true){
+    console.log(`
+    1 - Cadastrar Jogo
+    2 - Listar Jogos
+    3 - Atualizar Jogo
+    4 - Remover Jogo
+    5 - Sair
+    `);
+
+
+
+
+}
